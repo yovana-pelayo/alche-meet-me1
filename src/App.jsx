f88ae592f82dc81124f4abc85ab7fd4c42753b5b
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-04-28 22:44:45 -0700

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './views/Home/Home'
 import fetchUser from './services/user'
 
 function App() {
-  // inital value should match the data type of end value
+  // initial value should match the data type of end value
   const [user, setUser] = useState({})
 
   useEffect(() => {
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+describe('App', () => {
+  it('Should render the header', () => {
+    render(<App />)
+    const image = screen.getAllByAltText('Alchemy Logo')
+    // const headerText = await screen.getAllByLabelText('name!')
+    const header = screen.getByRole('banner')
 
+    expect(image).toBeInTheDocument()
+    // expect(headerText).toBeInTheDocument()
+    expect(header).toHaveStyle()
+  })
 })
